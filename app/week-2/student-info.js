0d0b517db1b9aa007e3d3c294ca6fcf187916522
antoniomacedo1 <antoniofilipe.thomazmacedo@edu.sait.ca>
2025-09-19 15:50:51 -0600

    Add week 2 files

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Antonio Macedo</p>
+      <p>
+        <Link href="https://github.com/antoniomacedo1" target="_blank">
+          My GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
